751b5aa78fb9c5643031a341a928ba63968b6895
Danilo Pianini <danilo.pianini@gmail.com>
2019-12-05 20:16:35 +0100

    Compute version with plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
     `java-library`
+    id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.2"
+}
+
+gitSemVer {
+    version = computeGitSemVer() // THIS IS MANDATORY, AND MUST BE LAST IN BLOCK
 }
