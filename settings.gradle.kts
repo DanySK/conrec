7c1c21d9577fe59cf2e833f9da8ef0b770ec0154
Danilo Pianini <danilo.pianini@unibo.it>
2023-06-22 13:11:25 +0200

    build: drop refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,20 @@
-import org.danilopianini.VersionAliases.justAdditionalAliases
-
 plugins {
-    id("de.fayard.refreshVersions") version "0.51.0"
+    id("com.gradle.enterprise") version "3.13.4"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.9"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
 }
 
 rootProject.name = "conrec"
-
-refreshVersions {
-    featureFlags {
-        enable(de.fayard.refreshVersions.core.FeatureFlag.LIBS)
-    }
-    extraArtifactVersionKeyRules = justAdditionalAliases
-}
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.danilopianini:refreshversions-aliases:+")
-    }
-}
