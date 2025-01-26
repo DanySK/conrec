7a5fc1938e6aeab3dcb378edd08c5c766d2a3d9c
Danilo Pianini <danilo.pianini@gmail.com>
2025-01-26 17:42:01 +0100

    build: migrate to develocity (#575)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    id("com.gradle.enterprise") version "3.19.1"
+    id("com.gradle.develocity") version "3.19.1"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.19"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishOnFailure()
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
     }
 }
 
